d58b6cabae05c3af15bc1a9ad8cc6cd40a931cd0
Sean <sstwunett@hotmail.com>
2023-04-17 11:49:34 +0200

    npm all to latest

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
 
 <template>
-  <RouterView />
+  <h1>Removed router</h1>
 </template>
+
+<script setup lang="ts"></script>
 
 <style scoped></style>
