4f75b79e0e03046984c02a880b1007de350f1f54
Sean <sstwunett@hotmail.com>
2023-04-20 22:45:01 +0200

    Bootstrap to windi

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap';
+import 'virtual:windi-base.css';
+import 'virtual:windi-components.css';
+import 'virtual:windi-utilities.css';
+import 'virtual:windi-devtools';
 
 const app = createApp(App)
 
